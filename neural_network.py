Bugfix: error in backpropagation algorithm
- In the derivation of the alg, I miscalculated the recurrence
  relation for dav_C. dav_C for the previous layer should be
  reset to W^t dot *dzv_C*, not W^t dot *dav_C*.
  This has been corrected, and tests were updated accordingly.
- Passes all tests.

neural_network.py
@@ -141,7 +141,7 @@ class NeuralNetwork(object):
             delta_W = -self.eta * np.outer(dzv_C, av_prev)
 
             # Compute new intermediary derivatives
-            dav_C = np.transpose(l.W).dot(dav_C)
+            dav_C = np.transpose(l.W).dot(dzv_C)
             dzv_C = dav_C * sigmoid_prime(av_prev)
 
             # Adjust biases and weights by their deltas

tests.py
@@ -357,7 +357,7 @@ class TestTrain(unittest.TestCase):
         y = 0
         examples = [(xv, y)]
 
-        av_new = np.array([0.53349089, 0.50197267])  # result of xv after training
+        av_new = np.array([0.53348918, 0.50197726])  # result of xv after training
         y_new = 0
 
         nn.train(examples)
@@ -375,7 +375,7 @@ class TestTrain(unittest.TestCase):
         y = 0
         examples = [(xv, y)]
 
-        av_new = np.array([0.53924222, 0.50779901, 0.50407205, 0.51082315])  # result of xv after training
+        av_new = np.array([0.53925307, 0.50780625, 0.50407204, 0.51082614])  # result of xv after training
         y_new = 0
 
         nn.train(examples)
@@ -400,7 +400,7 @@ class TestTrain(unittest.TestCase):
         y2 = 1
         examples = [(xv1, y1), (xv2, y2)]
 
-        av_new = np.array([0.51736309, 0.51714300])  # result of xv1 after training
+        av_new = np.array([0.51736881, 0.51714835])  # result of xv1 after training
         y_new = 0
 
         nn.train(examples)
@@ -420,7 +420,7 @@ class TestTrain(unittest.TestCase):
         y2 = 1
         examples = [(xv1, y1), (xv2, y2)]
 
-        av_new = np.array([0.52299694, 0.52276690, 0.48873272, 0.49528621])  # result of xv1 after training
+        av_new = np.array([0.52300829, 0.52278460, 0.48872059, 0.49527995])  # result of xv1 after training
         y_new = 0
 
         nn.train(examples)